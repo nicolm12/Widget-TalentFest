8a3215a74f7d2419553378ae2bbe6da169b861aa
unknown <bersiva@gmail.com>
2021-09-13 12:50:37 -0500

    Se añadio router y fonts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { Fragment } from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fragment>
+          <Router>
+            <Switch>
+              <Route>
+
+              </Route>
+            </Switch>
+          </Router>
+        </Fragment>
       </header>
     </div>
   );
