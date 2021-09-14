0e75ff500b285f058ba4ba0168ec98dcf66cfe52
leysanchezb92 <leysanchezb92@hotmail.com>
2021-09-13 23:41:57 -0500

    adding styles to formCalculator

--- src/components/FormCalculator.jsx
+++ src/components/FormCalculator.jsx
@@ -1,49 +1,61 @@
 import React from 'react'
 import "./components.css"
+import information from "../assets/information.png"
 
 
 const Form = (props) => {
-    return ( 
+  return (
     <div className="container">
-    <div className="form">
+      <div className="wrapper">
         <h2>{props.title}</h2>
-        <form action="#">
+        <form action="#" className="form">
+          <div className="divInfo">
             <label htmlFor="">{props.firstInput}</label>
-            <input type="range" min="-10" max="10" step="0.01"/>
+            <img src={information} alt="information" className="information" />
+          </div>
+          <h3>898989</h3>
+          <input type="range" min="-10" max="10" step="0.01" />
+          <div className="divInfo">
             <label htmlFor="">{props.secondInput}</label>
-            <select name="cars" id="cars" form="carform">
-                <option value="firstOption">{props.firstOption}</option>
-                <option value="secondOption">{props.secondOption}</option>
-            </select>
+            <img src={information} alt="information" className="information" />
+          </div>
+          <select name="cars" id="cars" form="carform">
+            <option value="firstOption">{props.firstOption}</option>
+            <option value="secondOption">{props.secondOption}</option>
+          </select>
+          <div className="divInfo">
             <label htmlFor="">{props.thirdInput}</label>
-            <input type="range" min="-10" max="10" step="0.01"/>
-        </form> 
-    </div>
+            <img src={information} alt="information" className="information" />
+          </div>
+          <h3>15</h3>
+          <input type="range" min="-10" max="10" step="0.01" />
+        </form>
+      </div>
 
-    <div className="results">
+      <div className="results">
         <div className="result">
           <p>{props.firstParagraph}</p>
           <h2>{props.valueOne}</h2>
           <img src="" alt="" />
           <p>{props.secondParagraph}</p>
         </div>
-        <div  className="result">
+        <div className="result">
           <p>{props.thirdParagraph}</p>
           <h2>{props.valuetwo}</h2>
           <p>{props.fourParagraph}</p>
         </div>
-        <div  className="result">
+        <div className="result">
           <h2>{props.valueThree}</h2>
           <p>{props.fiveParagraph}</p>
         </div>
-        <div  className="result">
+        <div className="result">
           <p>{props.sixParagraph}</p>
           <h2>{props.valueFour}</h2>
         </div>
-       
+
+      </div>
     </div>
-    </div>
-     );
+  );
 };
- 
+
 export default Form;
--- src/components/Grips.jsx
+++ src/components/Grips.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import Form from './FormCalculator'
 
-const Grips  = () => {
-    return ( 
-       <Form
-       title = {"Calcula tu precha pensiónal por incapacidad" }
-       firstInput = {"Tu ingreso actual"}
-       secondInput = {"Tu tipo de salario"}
-       firstOption = {"Integral"}
-       secondOption = {"No integral"}
-       thirdInput = {"Años que has cotizado a pensión"}
+const Grips = () => {
+  return (
+    <Form
+      title={"Calcula tu Brecha Pensiónal por Incapacidad"}
+      firstInput={"Tu ingreso actual"}
+      secondInput={"Tu tipo de salario"}
+      firstOption={"Integral"}
+      secondOption={"No integral"}
+      thirdInput={"Años que has cotizado a pensión"}
 
-       firstParagraph = {"Si llegarás a incapacitarte te faltarian"}
-       valueOne = {1000000}
-       secondParagraph = {"Todos los meses para mantener tu estandar de vida actual"}
-       thirdParagraph = {"Tu valor ideal de aseguramiento sera"}
-       valuetwo = {9000000}
-       fourParagraph = {"Por enfermedades"}
-       valueThree = {13000000000}
-       fiveParagraph = {"Por invalidez"}
-       sixParagraph = {"Este riesgo lo puedes cubrir por solo"}
-       valueFour = {70000}
-       >
-       </Form>
-     );
+      firstParagraph={"Si llegarás a incapacitarte te faltarian"}
+      valueOne={1000000}
+      secondParagraph={"Todos los meses para mantener tu estandar de vida actual"}
+      thirdParagraph={"Tu valor ideal de aseguramiento sera"}
+      valuetwo={9000000}
+      fourParagraph={"Por enfermedades"}
+      valueThree={13000000000}
+      fiveParagraph={"Por invalidez"}
+      sixParagraph={"Este riesgo lo puedes cubrir por solo"}
+      valueFour={70000}
+    >
+    </Form>
+  );
 }
- 
+
 export default Grips;
