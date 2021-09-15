65073504e56d19cb4c829de0f32a6d1ca5e528c0
leysanchezb92 <leysanchezb92@hotmail.com>
2021-09-15 11:04:26 -0500

    Borrado de react icons y responsive

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,50 +2,50 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Mainview from './Mainview';
 import styleDash from '../CSS/mainview.module.css'
-import ResponsiveMenu from 'react-responsive-navbar';
-import { FaBars,FaTimes } from 'react-icons/fa';
+// import ResponsiveMenu from 'react-responsive-navbar';
+// import { FaBars,FaTimes } from 'react-icons/fa';
 import { useState } from 'react';
 import Figuro from "../assets/Figuro Transparente.png"
 
 
 export default function Dashboard() {
-   const [open, setOpen] = useState(true)
+  // const [open, setOpen] = useState(true)
 
-    return(
-     <div className={styleDash.container}>
-          <div className={styleDash.navBar}>
-          <ResponsiveMenu
-              menuOpenButton={<FaBars/>}
-              menuCloseButton={<FaTimes/>}
-              changeMenuOn="2000px"
-              largeMenuClassName="large-menu-classname"
-              smallMenuClassName="small-menu-classname"
-              menu={
-                <ul>
-                  <li>Mis decisiones</li>
-                  <li>Productos</li>
-                  <li>Cursos y Webinars</li>
-                  <li>Calculadoras</li>
-                  <li>inicio</li>
-                </ul>
-              }
-            />
-           
-          <img src={Figuro} alt="Logo Figuro" className={styleDash.imgLogo}/> 
-          </div>
-          
-          <div className={styleDash.containerSidebar} >           
-            <Sidebar/>
-          </div>
-          <div className={styleDash.containerMainview}>
-            <Mainview/>
-          </div>
+  return (
+    <div className={styleDash.container}>
+      <div className={styleDash.navBar}>
+        {/* <ResponsiveMenu
+          menuOpenButton={<FaBars />}
+          menuCloseButton={<FaTimes />}
+          changeMenuOn="2000px"
+          largeMenuClassName="large-menu-classname"
+          smallMenuClassName="small-menu-classname"
+          menu={
+            <ul>
+              <li>Mis decisiones</li>
+              <li>Productos</li>
+              <li>Cursos y Webinars</li>
+              <li>Calculadoras</li>
+              <li>inicio</li>
+            </ul>
+          }
+        /> */}
+
+        <img src={Figuro} alt="Logo Figuro" className={styleDash.imgLogo} />
       </div>
-    )
-  }
-  
 
-    
+      <div className={styleDash.containerSidebar} >
+        <Sidebar />
+      </div>
+      <div className={styleDash.containerMainview}>
+        <Mainview />
+      </div>
+    </div>
+  )
+}
+
+
+
 
 
 //   return(
@@ -70,7 +70,7 @@ export default function Dashboard() {
 //                </ul>
 //              }
 //            />
-          
+
 //          <img src={Figuro} alt="Logo Figuro" className={styleDash.imgLogo}/> 
 //          </div>
 //          < div className={styleDash.containerMainview}>
