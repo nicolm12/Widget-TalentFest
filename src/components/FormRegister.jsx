706579fe71bccfb501e8a530e95db9cc923bbc98
CleidyBelizario <cleidybeliz.0891@gmail.com>
2021-09-15 11:56:01 -0500

    cambios en form Register

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import "./form.css"
 import Figuro from "../assets/Figuro Transparente.png"
+import { useState } from "react";
 
 
 
 const FormRegister = (props) => {
+
+    const [data, setData] = useState({
+        name: '',
+        lasName: '',
+        gender: '',
+        country: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+        terminos: '',
+    })
+
+    const handleInputChange = (event) => {
+        //console.log(event.target.value)
+        setData({
+            ...data,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    const enviarDatos = (event) =>{
+        event.preventDefault();
+        
+    }
+
+    const onChangeTerminos = (event) =>{
+      console.log(event.target.checked) 
+    }
+
     return (
         <>
             <img src={Figuro} alt="logo" className="logo" />
@@ -12,28 +42,21 @@ const FormRegister = (props) => {
                 <div className='grupoContainer'>
                     <h3>{props.title}</h3>
                     <p>{props.parrafo}</p>
-                    <form className='formLogin'>
+                    <form className='formLogin' onSubmit={enviarDatos} >
                         <label>{props.name}</label>
-                        <input type="text" name="Text Name" className='input' />
+                        <input type="text" name="name" className='input' onChange={handleInputChange} />
                         <label>{props.LastName}</label>
-                        <input type="text" name="Text LasName" className='input' />
+                        <input type="text" name="lasName" className='input' onChange={handleInputChange} />
                         <label>{props.gender}</label>
-                        <select name="gender" id="gender" form="gender" className='input'>
-                            <option value="oneOption">{props.oneOption}</option>
-                            <option value="twoOption">{props.twoOption}</option>
-                            <option value="threeOption">{props.threeOption}</option>
-                        </select>
+                        <input type="text" name="gender" className='input' placeholder="Hombre/Mujer" onChange={handleInputChange} />
                         <label>{props.country}</label>
-                        <input type="text" name="Text country" className='input' />
+                        <input type="text" name="country" className='input' onChange={handleInputChange} />
                         <label>{props.email}</label>
-                        <input type="email" name="email" id="email" placeholder="email" className='input' />
+                        <input type="email" name="email" id="email" placeholder="email" className='input' onChange={handleInputChange} />
                         <label>{props.password}</label>
-                        <input type="Password" name="Password" id="Password" placeholder="Password" className='input' />
-                        <label>{props.passwordConfirm}</label>
-                        <input type="PasswordConfirm" name="PasswordConfirm" id="PasswordConfirm" placeholder="Password Confirm" className='input' />
-                        <input type="checkbox" name="terminos" id="terminos"
-													/>
-					<label>{props.terminos}</label>
+                        <input type="Password" name="Password" id="Password" placeholder="Password" className='input' onChange={handleInputChange} />
+                        <input type="checkbox" name="terminos" id="terminos" onChange={onChangeTerminos}/> <label>{props.terminos}</label>
+                        <button className='btn'>{props.btn}</button>
                     </form>
                 </div>
             </div>
